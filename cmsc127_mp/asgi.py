a9ed642a3bfbcba7de9941471c91be4ac5307884
Adrian Santos <apsantos8@up.edu.ph>
2021-05-17 21:07:59 +0800

    Initial commit

---
+++ cmsc127_mp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cmsc127_mp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmsc127_mp.settings')
+
+application = get_asgi_application()
