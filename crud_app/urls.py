3f6e72c27fa3ffa714bdfec72378a1b52ea9dfb4
Adrian Santos <apsantos8@up.edu.ph>
2021-05-20 16:03:46 +0800

    created homepage, signIn, signUp

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+
+urlpatterns = [
     path('', views.home, name="home"),
-}
+    path('signIn/', views.signIn, name="signIn"),
+    path('signUp/', views.signUp, name="signUp")
+]
--- crud_app/views.py
+++ crud_app/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'crud_app/pages/home.html')
 
+def signIn(request):
+    return render(request, 'crud_app/pages/signIn.html')
+
+def signUp(request):
+    return render(request, 'crud_app/pages/signUp.html')
+
